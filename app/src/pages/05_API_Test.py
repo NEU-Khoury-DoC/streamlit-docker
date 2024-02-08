27f9ea219c015eaec72c5ba059cec9cad85102a3
Mark Fontenot <m.fontenot@northeastern.edu>
2024-02-08 06:10:11 -0500

    added error handling for rest api hit if data not accessible

--- app/src/pages/05_API_Test.py
+++ app/src/pages/05_API_Test.py
@@ -14,7 +14,11 @@ Simply retrieving data from a REST api running in a separate Docker Container.
 If the container isn't running, this will be very unhappy.  But the Streamlit app 
 should not totally die. 
 """
+data = {} 
+try:
+  data = requests.get('http://localhost:8989/data').json()
+except:
+  st.write("**Important**: Could not connect to sample api, so using dummy data.")
+  data = {"a":{"b": 123, "c": "hello"}, "z": {"b": 456, "c": "goodbye"}}
 
-data = requests.get('http://localhost:8989/data').json()
-
-st.write(data)
+st.dataframe(data)
